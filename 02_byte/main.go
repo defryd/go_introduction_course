0b4ec8344c39ffbafa9942f5aecbdcfdb0e9dcd3
‘Freddy <elipson39z@gmail.com>
2025-04-08 20:20:14 -0500

    variables

--- 02_byte/main.go
+++ 02_byte/main.go
@@ -5,7 +5,13 @@ import "fmt"
 func main() {
 	var A byte = 'A'
 	var a byte = 'a'
+	var R byte = 82
+	var s byte = 115
 
+	fmt.Println()
+	fmt.Println("Value in ASCII Code:")
 	fmt.Println("A:", A)
 	fmt.Println("B:", a)
+	fmt.Println("R:", R)
+	fmt.Println("s:", s)
 }
