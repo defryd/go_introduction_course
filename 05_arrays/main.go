b367e5f3e2f919e6316c97fa118bc05459e08d37
‘Freddy <elipson39z@gmail.com>
2025-04-09 20:29:41 -0500

    slice section

--- 05_arrays/main.go
+++ 05_arrays/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	fmt.Println("ARRAYS")
 	var myIntVar int
 	fmt.Println("myIntVar:", myIntVar)
 	fmt.Printf("type: %T, bytes: %d, bits: %d\n", myIntVar, unsafe.Sizeof(myIntVar), unsafe.Sizeof(myIntVar)*8)
@@ -24,4 +25,38 @@ func main() {
 	fmt.Println("Size of myArrayVar:", len(myArrayVar))
 	fmt.Printf("type: %T, bytes: %d, bits: %d\n", myArrayVar, unsafe.Sizeof(myArrayVar), unsafe.Sizeof(myArrayVar)*8)
 
+	fmt.Println()
+	fmt.Println("SLICE")
+
+	// Un slice es un puntero a un array
+	var slice1 []int
+	fmt.Printf("size of slice1: %d, value %v\n", len(slice1), slice1)
+
+	slice1 = append(slice1, 10, 20, 30, 40, 50)
+	fmt.Printf("size of slice1: %d, value %v\n", len(slice1), slice1)
+
+	//toma los valores del 5 al 9 de myArrayVar
+	mySlice := myArrayVar[2:4]
+	fmt.Println(mySlice)
+	fmt.Println("Size of mySlice: ", len(mySlice))
+
+	fmt.Println(&myArrayVar[2])
+	fmt.Println(&mySlice[0])
+
+	fmt.Println("myArrayVar antes ", myArrayVar)
+	fmt.Println("mySlice antes ", mySlice)
+
+	mySlice[0] = 80
+	mySlice[1] = 100
+
+	fmt.Println("myArrayVar despues ", myArrayVar)
+	fmt.Println("mySlice despues ", mySlice)
+
+	fmt.Println("del inicio al indc 4", myArrayVar[:4])
+	fmt.Println("del indc 2 al final", myArrayVar[2:])
+
+	// otra forma de instanciar un slice
+	slice := make([]int, 3)
+	fmt.Println(slice)
+	fmt.Printf("size: %d, value %v\n", len(slice), slice)
 }
