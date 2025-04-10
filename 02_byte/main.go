ae3d78dc018b3be4924aee10f3fc6e963cf26e5b
‘Freddy <elipson39z@gmail.com>
2025-04-09 19:52:14 -0500

    final variables

--- 02_byte/main.go
+++ 02_byte/main.go
@@ -8,10 +8,23 @@ func main() {
 	var R byte = 82
 	var s byte = 115
 
+	// crear un vector
+	vector := []byte{65, 97, 82, 115}
+
 	fmt.Println()
 	fmt.Println("Value in ASCII Code:")
 	fmt.Println("A:", A)
 	fmt.Println("B:", a)
 	fmt.Println("R:", R)
 	fmt.Println("s:", s)
+	fmt.Println("vector", vector)
+
+	fmt.Println()
+	fmt.Println("Value in String:")
+	fmt.Println("A:", string(A))
+	fmt.Println("B:", string(a))
+	fmt.Println("R:", string(R))
+	fmt.Println("s:", string(s))
+	fmt.Println("vector", string(vector))
+
 }
